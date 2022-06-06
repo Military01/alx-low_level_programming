3ab4d1d3e5aa74c5fdcc049aa876dd76944caafd
Military01 <chrisezereonye@gmail.com>
2022-06-06 15:18:48 -0700

    ook

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
-///*/*
-/* main - program prints with function puts
-/*
-/*Return: Always 0
-/*//
+/**
+* main - program prints with function puts
+*
+*Return: Always 0
+*/
 int main(void)
 {
+
+
+pu
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
