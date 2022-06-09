3a9c6dae668f4f25e91c369031ea23d3b0d38033
Military01 <chrisezereonye@gmail.com>
2022-06-09 07:52:14 -0700

    part 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - func that prints 10 times the numbers 0 to 14 on different lines
+ * more_numbers - func that prints 10 times the numbers, from 0 to 14
  *
  */
 
