15169c41fb28a5c46a2927aa26fae784243d5ff6
Military01 <chrisezereonye@gmail.com>
2022-06-13 13:15:19 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strlen - return length of string
+ * *@s: pointer to char
+ *
+ * Return: return an integer
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
